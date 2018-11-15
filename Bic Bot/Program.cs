765f482ce9e6f28064ae5f39655a5aad5cb6ff50
The Bic Pen <mashkal2000@gmail.com>
2018-11-14 20:07:22 -0500

    Add project files.

---
+++ Bic Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bic_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
