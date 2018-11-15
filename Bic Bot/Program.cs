89a1ca167b4075c1513885c3b7141676d5bec44c
The Bic Pen <mashkal2000@gmail.com>
2018-11-14 20:25:33 -0500

    Inital commit. Added ignore to token file.

--- Bic Bot/Program.cs
+++ Bic Bot/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
 
 namespace Bic_Bot
 {
     class Program
     {
         static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+
+        }
+
+
+        private Task Log(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
         }
     }
 }
