8ac57579f08388b870eb39e0110c5bd3f2d6997d
The Bic Pen <mashkal2000@gmail.com>
2018-11-14 21:56:33 -0500

    added functionality

--- Bic Bot/Program.cs
+++ Bic Bot/Program.cs
@@ -1,23 +1,89 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 using System.Threading.Tasks;
-using Discord;
 
-namespace Bic_Bot
+namespace MyBot
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        => new Program().MainAsync().GetAwaiter().GetResult();
+        string commandString = ";";
+        Dictionary<string, string> callList;
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
 
         public async Task MainAsync()
         {
+            var client = new DiscordSocketClient();
+
+            client.Log += Log;
+            client.MessageReceived += MessageReceived;
+
+            string token = System.IO.File.ReadAllText("token.txt"); 
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if(message.Content.StartsWith(commandString))
+            {
+                await ParseCommand(message);
+            }
+        }
+
+        private async Task ParseCommand(SocketMessage message)
+        {
+            switch (message.Content)
+            {
+                case ";ping":
+                    await message.Channel.SendMessageAsync("Pong!");
+                    break;
+                case ";version":
+                    await message.Channel.SendMessageAsync(Assembly.GetExecutingAssembly().GetName().Version.ToString());
+                    break;
+                case ";callme":
+                    await CallMe(message);
+                    break;
+                case ";myName":
+
+                case ";isbot":
+                    await message.Channel.SendMessageAsync("yes");
+                    break;
+                default:
+                    await message.Channel.SendMessageAsync("Invalid command.");
+                    break;
+            }
 
         }
 
-
-        private Task Log(LogMessage message)
+        private async Task CallMe(SocketMessage message)
         {
-            Console.WriteLine(message.ToString());
+            callList.Add(message.Author.Username, message.Content);
+            await message.Channel.SendMessageAsync(String.Format(@"{0}, I will call you {1}", message.Author.Username, message.Content));
+        }
+
+        private async Task GetName(SocketMessage message)
+        {
+            if(callList.ContainsKey(message.Author.Username))
+            {
+                await message.Channel.SendMessageAsync("Your name is " + callList[message.Author.Username] + ".");
+            }
+            else
+            {
+                await message.Channel.SendMessageAsync("I don't know your name.");
+            }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
         }
     }
